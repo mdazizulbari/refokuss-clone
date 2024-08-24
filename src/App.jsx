92ac10412cb1f47665650ec00972c1d4c8d2e78c
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-24 06:06:15 +0600

    installed locomotive

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import Cards from "./Components/Cards"
-import Footer from "./Components/Footer"
-import Marquees from "./Components/Marquees"
-import Navbar from "./Components/Navbar"
-import Products from "./Components/Products"
-import Stripes from "./Components/Stripes"
-import Work from "./Components/Work"
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
+import Marquees from "./Components/Marquees";
+import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
+import Stripes from "./Components/Stripes";
+import Work from "./Components/Work";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
-<div className="w-full text-white bg-black">
-      <Navbar/>
-      <Work/>
-      <Stripes/>
-      <Products/>
-      <Marquees/>
-      <Cards/>
-      <Footer/>
+    <div className="w-full text-white bg-black">
+      <Navbar />
+      <Work />
+      <Stripes />
+      <Products />
+      <Marquees />
+      <Cards />
+      <Footer />
     </div>
-  )
+  );
 }
-export default App
+export default App;
