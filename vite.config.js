41f14491db514b8925db70319ca26349013e520d
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-27 23:37:44 +0600

    idk rn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/refokuss-clone',
+  base: '/refokuss-clone/',
   plugins: [react()],
 })
